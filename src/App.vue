aaa5e76c898e6c9c0e0564d0e987861043e32a60
Fred Zhang <wb-zq277905@alibaba-inc.com>
2022-06-09 09:54:58 +0800

    feature-项目初步构建vue+vite+antDesign/1.0.0

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+//导入模块
+//import HelloWorld from './components/HelloWorld.vue'
+import Home from './view/home/index.vue'
+
+//声明变量
+let name = 'fred';
+
+function fn (str) {
+  console.log(str);
+  console.log(name);
+};
+
+</script>
+
+<template>
+  <Home />
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" :name="name" :fn="fn" /> -->
+</template>
+
+<style>
+html,body,#app {
+  height: 100%;
+  background-color: pink;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ msg }} {{ name }}</h1>
+    <button type="button" @click="count++">count is: {{ count }}</button>
+    <br />
+    <br /><a-checkbox :checked="count % 2 == 0 ? true : false ">Checkbox</a-checkbox>
+    <br />
+    <br /><a-button type="primary">Primary Button</a-button>
+    <br /><a-button @click="fn(1)">Default Button</a-button>
+    <br /><a-button type="dashed">Dashed Button</a-button>
+    <br /><a-button type="text">Text Button</a-button>
+    <br /><a-button type="link">Link Button</a-button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+defineProps({
+  msg: String,
+  name: String,
+  fn: Function,
+})
+const count = ref(0)
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
